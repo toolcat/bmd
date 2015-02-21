3a1c6b3c037a82760ede19b14cbd004ede371e34
Daniel Kovacs <lion.myip@gmail.com>
2015-02-21 17:59:14 +0100

    Test application

---
+++ src/main/java/com/toolcat/bmd/Starter.java
@@ -0,0 +1,10 @@
+package com.toolcat.bmd;
+
+import javax.swing.JOptionPane;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "It is runing!", "Test", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
