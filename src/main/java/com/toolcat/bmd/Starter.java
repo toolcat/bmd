f5c5ba046004d2c2c3d6eb1df8103995899e008e
Daniel Kovacs <lion.myip@gmail.com>
2015-02-22 21:59:57 +0100

    Alfa frame for all display

--- src/main/java/com/toolcat/bmd/Starter.java
+++ src/main/java/com/toolcat/bmd/Starter.java
@@ -1,10 +1,28 @@
 package com.toolcat.bmd;
 
-import javax.swing.JOptionPane;
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
 
 public class Starter {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "It is runing!", "Test", JOptionPane.INFORMATION_MESSAGE);
+		GraphicsEnvironment g = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] devices = g.getScreenDevices();
+		
+		for (GraphicsDevice device : devices) {
+			System.out.println(device.isWindowTranslucencySupported(java.awt.GraphicsDevice.WindowTranslucency.TRANSLUCENT));
+			System.out.println(device.getDefaultConfiguration().getBounds());
+			Rectangle rectangle = device.getDefaultConfiguration().getBounds();
+			JFrame frame = new JFrame();
+			frame.getContentPane().setBackground(Color.BLUE);
+			frame.setUndecorated(true);
+			frame.setOpacity(0.01f);
+			frame.setBounds(rectangle);
+			frame.setVisible(true);
+		}
 	}
 }
